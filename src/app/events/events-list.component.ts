1bdaf9025c5bcc7f112e14b8ef44028fb5106eea
mdx919 <thanvirdx919@gmail.com>
2019-06-11 23:56:26 -0400

    added @input @output and event emitter

---
+++ src/app/events/events-list.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '<events-list></events-list>',
+    template: `
+        <div>
+        <h2>Upcoming Angular Events</h2>
+        <img src="/assets/images/basic-shield.png" alt="">
+        <hr>
+        <event-thumbnail [event]="event1" (eventClick)="handleEventClicked($event)">
+        </event-thumbnail>
+        </div>
+    `
+})
+export class EventsListComponent {
+    event1 = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '9/3/2019',
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: '/assets/images/angularconnect-shield.png',
+        location: {
+            address: '1044 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
+
+    handleEventClicked(data) {
+        console.log('recieved:', data);
+        
+    }
+    
+}
