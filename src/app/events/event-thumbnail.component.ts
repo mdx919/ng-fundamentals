c55bba64f912975fbe011b9b133dd4afcf8e29e1
mdx919 <thanvirdx919@gmail.com>
2019-06-13 00:22:27 -0400

    ng directives

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -5,20 +5,40 @@ import { Component, Input } from '@angular/core';
     template: `
         <div class="well hoverwall thumbnail">
             <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>              
-            <div>Time: {{event.time}}</div>              
-            <div>Price: \${{event.price}}</div>              
-            <div>
-                <span>Location: {{event.location.address}} </span>
-                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <div [ngStyle]="getDateStyle()">Date: {{event?.date}}</div>              
+            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>              
+            <div>Price: \${{event.price}}</div>          
+            <div [hidden]="!event?.location">
+                <span>Location: {{event?.location.address}} </span>
+                <span class="pad-left">{{event?.location?.city}}, {{event.location.country}}</span>
             </div>           
         </div>
     `,
     styles: [`
+        .green { color: #003300 !important; }
+        .bold { font-weight: bold; }
+        .thumbnail { min-height: 250px; }
         .pad-left { margin-left: 0.5em; }
         .well div { color: #bbb; } 
     `]
 })
 export class EventThumbnailComponent {
     @Input() event:any;
+
+    getStartTimeClass() {
+        if (this.event && this.event.time === '8:00 am')
+            return ['green', 'bold']
+        return []
+    }
+
+    getDateStyle(): any {
+        if (this.event && this.event.date)
+            return { color: 'yellow', 'font-weight': 'bold' }
+        return {}
+    }
 }
--- src/app/nav/navbar.component.ts
+++ src/app/nav/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     styles: [`
         .nav.navbar-nav { font-size: 15px; }
         #searchForm { margin-right: 100px; }
-        @media (max-width: 1400px) { #searchform {display: none} }
+        @media (max-width: 1200px) { #searchform {display: none} }
     `]
 })
 export class NavbarComponent {
